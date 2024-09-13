f77471513feb6d407277295f309aca6a3cb64b37
krishnaGauss <goswamikrishna549@gmail.com>
2024-09-13 18:52:40 +0530

    added route and react components

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Button} from "@/components/ui/button"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Auth from "./pages/auth"
+import Chat from './pages/chat'
+import Profile from './pages/profile'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/auth" element= { <Auth />}/>
+      <Route path="/chat" element= { <Chat />}/>
+      <Route path="/profile" element= { <Profile />}/>
+      {/* serves as a wildcard route for redirecting typos to auth */}
+      <Route path="*" element ={<Navigate to={"/auth"} />} /> 
+      </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
