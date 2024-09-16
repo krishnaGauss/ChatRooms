dabdd1269e93a754097561b385fb162a172068f0
krishnaGauss <goswamikrishna549@gmail.com>
2024-09-16 23:05:23 +0530

    Database integration and MVC functionality added

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN], //frontend urls which send requests
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true, //enable cookies
+  })
+);
+
+app.use(cookieParser()); //getting cookies
+app.use(express.json());
+
+const server = app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+mongoose
+  .connect(databaseURL)
+  .then(() => {
+    console.log("DB Running Successfully");
+  })
+  .catch((err) => console.log(err.message));
